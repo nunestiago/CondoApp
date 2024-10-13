dd1695eed4b7e036707d9f7b0c8949ce7d938d6a
Tiago Nunes <ntiagon@gmail.com>
2024-10-13 13:18:47 -0300

    Remove unused configuration files and add new domain value objects for product and order management

--- apps/client-ionic/src/app/app.routes.ts
+++ apps/client-ionic/src/app/app.routes.ts
@@ -2,11 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
-    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage),
+    path: 'a',
+    loadComponent: () => import('./login/login.page').then((m) => m.LoginPage)
   },
   {
-    path: 'a',
-    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
-  },
+    path: '',
+    loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes)
+  }
 ];
